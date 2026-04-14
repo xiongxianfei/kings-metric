1aa64811824190df5d31a949d02f0d55d510cb75
xiongxianfei <xiongxianfei@foxmail.com>
2026-04-14 13:09:36 +0300

    Fix Room code generation wiring

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application") version "9.1.0" apply false
+    id("com.google.devtools.ksp") version "2.1.20-2.0.1" apply false
     id("org.jetbrains.kotlin.jvm") version "2.1.20" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.20" apply false
 }
